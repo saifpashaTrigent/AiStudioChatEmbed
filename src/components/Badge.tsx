ceaf87e8cccf337c903fe0ec060383feba46ddfe
alsaif1431 <saifpashanaaz@gmail.com>
2025-03-20 12:11:40 +0530

    sa

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -51,7 +51,7 @@ export const Badge = (props: Props) => {
           {props.footer?.text ?? 'Powered by'}
           <a
             ref={liteBadge}
-            href={props.footer?.companyLink ?? 'https://trigent.com'}
+            href={props.footer?.companyLink ?? 'https://trigent.com/'}
             target="_blank"
             rel="noopener noreferrer"
             class="lite-badge"
